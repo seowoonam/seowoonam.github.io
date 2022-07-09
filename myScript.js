da3095bb025fdab7b1e6ef631f3e51cb088d438c
seowoonam <97seowoo@gmail.com>
2022-07-09 22:26:05 +0900

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -1,5 +1,9 @@
 //Make the DIV element draggagle:
-dragElement(document.getElementsByClassName("orange"));
+var draggableElements = document.getElementsByClassName("box");
+
+for(var i = 0; i < draggableElements.length; i++){
+    dragElement(draggableElements[i]);
+}
 
 function dragElement(elmnt) {
   var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
