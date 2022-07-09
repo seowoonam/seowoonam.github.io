56ab24d5130bbd98844a7d5ca030f606f002373e
seowoonam <97seowoo@gmail.com>
2022-07-09 21:21:57 +0900

    create contentcard

--- myScript.js
+++ myScript.js
@@ -35,4 +35,12 @@ function dragElement(elmnt) {
     document.onmouseup = null;
     document.onmousemove = null;
   }
+var closebtns = document.getElementsByClassName("close");
+var i;
+
+for (i = 0; i < closebtns.length; i++) {
+  closebtns[i].addEventListener("click", function() {
+    this.parentElement.style.display = 'none';
+  });
+}
 }
