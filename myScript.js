9e7a7e713ad512b39ee4075a8b2503a8b96dc04d
Lenny Martinez Dominguez <lennymartinezd@gmail.com>
2022-10-11 10:46:40 +0200

    clean up code

--- myScript.js
+++ myScript.js
@@ -1,18 +1,20 @@
-window.onload=changeFilterStatus(1);
-window.onload=changeFilterStatus(2);
-window.onload=changeFilterStatus(3);
+// window.onload = changeFilterStatus(1);
+// window.onload = changeFilterStatus(2);
+// window.onload = changeFilterStatus(3);
 
-//Make the DIV element draggagle:
-var draggableElements = document.getElementsByClassName("box");
-
-for(var i = 0; i < draggableElements.length; i++){
-    dragElement(draggableElements[i]);
+//Make the DIV element draggable:
+var draggableElements = document.getElementsByClassName('box');
+for (var i = 0; i < draggableElements.length; i++) {
+  dragElement(draggableElements[i]);
 }
 
 function dragElement(elmnt) {
-  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  var pos1 = 0,
+    pos2 = 0,
+    pos3 = 0,
+    pos4 = 0;
   elmnt.onmousedown = dragMouseDown;
-  
+  alert('test');
 
   function dragMouseDown(e) {
     e = e || window.event;
@@ -34,8 +36,8 @@ function dragElement(elmnt) {
     pos3 = e.clientX;
     pos4 = e.clientY;
     // set the element's new position:
-    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+    elmnt.style.top = elmnt.offsetTop - pos2 + 'px';
+    elmnt.style.left = elmnt.offsetLeft - pos1 + 'px';
   }
 
   function closeDragElement() {
@@ -44,72 +46,63 @@ function dragElement(elmnt) {
     document.onmousemove = null;
   }
 
-var closebtns = document.getElementsByClassName("close");
+  var closebtns = document.getElementsByClassName('close');
 
-for (var i = 0; i < closebtns.length; i++) {
-  closebtns[i].addEventListener("click", function() {
-    this.parentElement.style.display = 'none';
-  });
-}
+  for (var i = 0; i < closebtns.length; i++) {
+    closebtns[i].addEventListener('click', function () {
+      this.parentElement.style.display = 'none';
+    });
+  }
 }
 
 function toggleFilter(type) {
-	if (type === 1) {
-      var cardtype = "workcard";
-    }
-    else if (type === 2) {
-      var cardtype = "experimentcard";
-    }
-    else if (type === 3) {
-      var cardtype = "writingcard";
-    }
-    var filteredElements = document.getElementsByClassName(cardtype);
-  	for (var i = 0; i < filteredElements.length; i++) {
-      var x = filteredElements[i];
-      if (x.style.display === "none") {
-          x.style.display = "block";
-        } else {
-          x.style.display = "none";
-        }
-  	}
+  if (type === 1) {
+    var cardtype = 'workcard';
+  } else if (type === 2) {
+    var cardtype = 'experimentcard';
+  } else if (type === 3) {
+    var cardtype = 'writingcard';
   }
-
-
+  var filteredElements = document.getElementsByClassName(cardtype);
+  for (var i = 0; i < filteredElements.length; i++) {
+    var x = filteredElements[i];
+    if (x.style.display === 'none') {
+      x.style.display = 'block';
+    } else {
+      x.style.display = 'none';
+    }
+  }
+}
 
 function changeFilterStatus(type) {
   if (type === 1) {
-  	var filterButton = document.getElementById('workfilter'); 
-    if (filterButton.style.backgroundColor === "rgb(81, 70, 67)") {
-      filterButton.style.backgroundColor = "#B9B7B7";
-      filterButton.innerHTML = "WORK&nbsp;&nbsp;&#43;";
+    var filterButton = document.getElementById('workfilter');
+    if (filterButton.style.backgroundColor === 'rgb(81, 70, 67)') {
+      filterButton.style.backgroundColor = '#B9B7B7';
+      filterButton.innerHTML = 'WORK&nbsp;&nbsp;&#43;';
+    } else {
+      filterButton.style.backgroundColor = '#514643';
+      filterButton.innerHTML = 'WORK&nbsp;&nbsp;&#8722; ';
     }
-    else {
-        filterButton.style.backgroundColor = "#514643";
-        filterButton.innerHTML = "WORK&nbsp;&nbsp;&#8722; ";
-      }
   }
   if (type === 2) {
-    var filterButton = document.getElementById('experimentfilter'); 
-    if (filterButton.style.backgroundColor === "rgb(81, 70, 67)") {
-      filterButton.style.backgroundColor = "#B9B7B7";
-      filterButton.innerHTML = "EXPERIMENT&nbsp;&nbsp;&#43;";
+    var filterButton = document.getElementById('experimentfilter');
+    if (filterButton.style.backgroundColor === 'rgb(81, 70, 67)') {
+      filterButton.style.backgroundColor = '#B9B7B7';
+      filterButton.innerHTML = 'EXPERIMENT&nbsp;&nbsp;&#43;';
+    } else {
+      filterButton.style.backgroundColor = '#514643';
+      filterButton.innerHTML = 'EXPERIMENT&nbsp;&nbsp;&#8722; ';
     }
-    else {
-        filterButton.style.backgroundColor = "#514643";
-        filterButton.innerHTML = "EXPERIMENT&nbsp;&nbsp;&#8722; ";
-      }
   }
   if (type === 3) {
     var filterButton = document.getElementById('writingfilter');
-    if (filterButton.style.backgroundColor === "rgb(81, 70, 67)") {
-      filterButton.style.backgroundColor = "#B9B7B7";
-      filterButton.innerHTML = "WRITING&nbsp;&nbsp;&#43;";
+    if (filterButton.style.backgroundColor === 'rgb(81, 70, 67)') {
+      filterButton.style.backgroundColor = '#B9B7B7';
+      filterButton.innerHTML = 'WRITING&nbsp;&nbsp;&#43;';
+    } else {
+      filterButton.style.backgroundColor = '#514643';
+      filterButton.innerHTML = 'WRITING&nbsp;&nbsp;&#8722; ';
     }
-    else {
-        filterButton.style.backgroundColor = "#514643";
-        filterButton.innerHTML = "WRITING&nbsp;&nbsp;&#8722; ";
-      }
   }
-  
 }
-
